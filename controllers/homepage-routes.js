618632b3cde147f5a1857bee24fcc70fae25d07f
Ortal Wikoff <owikoff@gmail.com>
2022-08-11 13:38:57 -0400

    created main2 handlebar so we will see only home and signup at navbar

--- controllers/homepage-routes.js
+++ controllers/homepage-routes.js
@@ -1,7 +1,8 @@
 const router = require('express').Router();
 
 router.get('/', async (req,res) => {
-    res.render('homePage');
+    res.render('homePage', {layout: 'main2'});
 })
 
 module.exports = router;
+
