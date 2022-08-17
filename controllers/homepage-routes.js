5e20005218e3c7ce6504c8185d2c4772a18c6cb0
steven rosa <scrosa13@outlook.com>
2022-08-17 19:54:29 -0400

    updating sign up and login

--- controllers/homepage-routes.js
+++ controllers/homepage-routes.js
@@ -197,7 +197,7 @@ router.get('/', withAuth, async (req, res) => {
 router.get('/login', (req, res) => {
     // If a session exists, redirect the request to the homepage
     if (req.session.logged_in) {
-      res.redirect('/');
+      res.redirect('/myPage');
       return;
     }
   
