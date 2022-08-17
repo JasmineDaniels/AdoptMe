b753703aa05c248b1f51167305380fe036f7c72a
steven rosa <scrosa13@outlook.com>
2022-08-17 19:13:53 -0400

    updating login

--- seeds/users_seeds.js
+++ seeds/users_seeds.js
@@ -3,22 +3,27 @@ const { User } = require('../models');
 
 const UserData = [
   {
+    "username": "Ismael",
     "email": "lelj28819@gmail.com",
     "password": "password12345"
   },
   {
+    "username": "Max",
     "email": "max.mcdonough@gmail.com",
     "password": "password12345"
   },
   {
+    "username": "Jasmine",
     "email": "create.jasminedaniels@gmail.com",
     "password": "password12345"
   },
   {
+    "username": "Steve",
     "email": "scrosa13@outlook.com",
     "password": "password12345"
   },
   {
+    "username": "Ortal",
     "email": "owikoff@gmail.com",
     "password": "password12345"
   }
