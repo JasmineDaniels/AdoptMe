607cf8ef883d0f61e6a5ddb3f5d437f552eef14e
Jasmine Daniels <create.jasminedaniels@gmail.com>
2022-08-19 16:50:09 -0400

    form submit works --wip

---
+++ public/JavaScript/form-submit.js
@@ -0,0 +1,86 @@
+//const { response } = require("express");
+
+// const { response } = require("express");
+
+console.log('I am connected')
+
+document.querySelector('#submit-form').addEventListener('click', (event) => {
+    event.preventDefault();
+
+    console.log('I was clicked')
+
+    const name = document.getElementsByTagName('input')[0].value;
+    console.log(name)
+    const email = document.getElementsByTagName('input')[1].value;
+    console.log(email)
+    const subject = document.getElementsByTagName('input')[2].value;
+    console.log(subject)
+    const message = document.getElementsByTagName('textarea')[0].value;
+    console.log(message)
+    
+        // const name = document.querySelector('#name').value;
+        // console.log(name)
+        // const email = document.querySelector('#email').value;
+        // console.log(email)
+        // const subject = document.querySelector('#subject').value;
+        // console.log(subject)
+        // const message = document.querySelector('#message').value;
+        // console.log(message)
+
+        fetch('https://formsubmit.co/ajax/77706fa8e92bcf72bf399319f99f3ebd', {
+            method: "POST",
+            headers: {
+                'Content-Type': 'application/json',
+                'Accept': 'application/json'
+            },
+            body: JSON.stringify({
+                name,
+                email,
+                subject,
+                message,
+            })
+        })
+        .then(response => response.json())
+        .then(data => console.log(data))
+        .catch(error => console.log(error))
+
+
+        // const feedback = response.json(submitForm)
+    
+    
+})
+
+
+
+
+// document.querySelector('.contact-forms').forEach(form => {
+    
+//     const submitBtn = form.querySelector('#submit-form')
+
+//     submitBtn.addEventListener('click', (event) => {
+//         event.preventDefault();
+
+//         const name = document.getElementsByTagName('input')[0].value;
+//         const email = document.getElementsByTagName('input')[1].value;
+//         const subject = document.getElementsByTagName('input')[2].value;
+//         const message = document.getElementsByTagName('input')[3].value;
+        
+
+//         fetch('https://formsubmit.co/el/36cdf350c1201af95a6069bf6ccc4a84', {
+//             method: "POST",
+//             headers: {
+//                 'Content-Type': 'application/json',
+//                 'Accept': 'application/json'
+//             },
+//             body: JSON.stringify({
+//                 name,
+//                 email,
+//                 subject,
+//                 message,
+//             })
+//         })
+//         .then(response => response.json())
+//         .then(data => console.log(data))
+//         .catch(error => console.log(error))
+//     })
+// })
